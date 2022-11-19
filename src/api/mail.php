4338c9f4c9b3722a6dd427364599de33f3f605c2
novokshenov <novokshenov@flagstudio.ru>
2022-11-19 20:34:37 +0100

    wip

--- src/api/mail.php
+++ src/api/mail.php
@@ -4,16 +4,16 @@ $email = trim($_POST['email']);
 $phone = trim($_POST['phone']);
 $message = trim($_POST['message']);
 
-$fromMail = 'admin@victor-nov.ru';
-$fromName = 'victor-nov.ru Форма';
+$fromMail = 'admin@victor-nov-portfolio.netlify.app';
+$fromName = 'Contact Form';
 
 $emailTo = 'victornov@bk.ru';
-$subject = 'Форма обратной связи victor-nov.ru';
+$subject = 'Форма обратной связи с сайта';
 $subject = '=?utf-8?b?'. base64_encode($subject) .'?=';
 $headers = "Content-type: text/plain; charset=\"utf-8\"\r\n";
 $headers .= "From: ". $fromName ." <". $fromMail ."> \r\n";
 
-$body = "Получено письмо с сайта victor-nov.ru \n Имя: $name\nТелефон: $phone \n E-mail: $email\nСообщение: $message";
+$body = "Получено письмо с сайта victor-nov-portfolio.netlify.app \n Имя: $name\nТелефон: $phone \n E-mail: $email\nСообщение: $message";
 
 if (strlen($email) > 0) {
     $mail = mail($emailTo, $subject, $body, $headers, '-f'. $fromMail );
