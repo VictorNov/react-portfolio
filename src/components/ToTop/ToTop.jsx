29a8434ebac6cad77f455879342aec0592ebbcce
novokshenov <novokshenov@flagstudio.ru>
2022-11-20 17:10:11 +0100

    implemented page 404

--- src/components/ToTop/ToTop.jsx
+++ src/components/ToTop/ToTop.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import './ToTop.scss'
+import React from 'react';
+import './ToTop.scss';
 
 const ToTop = () => {
-  return (
-    <button
-      className="btn-top"
-      onClick={() => window.scrollTo(0, 0)}
-    >
-      <i className="ri-arrow-up-line" />
-    </button>
-  )
-}
+    return (
+        <button
+            className="btn-top"
+            onClick={() => window.scrollTo(0, 0)}
+        >
+            <i className="ri-arrow-up-line"/>
+        </button>
+    );
+};
 
-export default ToTop
+export default ToTop;
