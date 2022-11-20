198e54a63442a930fb1c1a8c233af0bd0124b641
novokshenov <novokshenov@flagstudio.ru>
2022-11-20 15:40:27 +0100

    refactoring Portfolio WIP

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,104 +1,107 @@
-import React, {useState, useEffect} from 'react'
-import { Rings } from 'react-loader-spinner'
+import React, { useState, useEffect } from 'react';
+import { Rings } from 'react-loader-spinner';
 
-import { PortfolioItem } from '../components'
-import './Portfolio.scss'
-import { client } from '../../client'
+import { PortfolioItem } from '../components';
+import './Portfolio.scss';
+import { client } from '../../client';
 
 const Portfolio = () => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [allTags, setAllTags] = useState([])
-  const [portfolioItems, setPortfolioItems] = useState([])
-  const [filter, setFilter] = useState(null)
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ allTags, setAllTags ] = useState([]);
+    const [ portfolioItems, setPortfolioItems ] = useState([]);
+    const [ filter, setFilter ] = useState(null);
 
-  useEffect(() => {
-    const tagsQuery = '*[_type == "tag"]'
-    const portfolioQuery = '*[_type == "projects"] | order(_createdAt desc)'
+    useEffect(() => {
+        const tagsQuery = '*[_type == "tag"]';
+        const portfolioQuery = '*[_type == "projects"] | order(_createdAt desc)';
 
-    client.fetch(tagsQuery)
-      .then(data => {
-        setAllTags(data)
-      })
+        client.fetch(tagsQuery)
+            .then(data => {
+                setAllTags(data);
+            });
 
-    client.fetch(portfolioQuery)
-      .then(data => {
-        setPortfolioItems(data)
-        setIsLoading(false)
-      })
-  }, [])
+        client.fetch(portfolioQuery)
+            .then(data => {
+                setPortfolioItems(data);
+                setIsLoading(false);
+            });
+    }, []);
 
-  useEffect(() => {
-    let query
+    useEffect(() => {
+        let query;
 
-    setIsLoading(true)
+        setIsLoading(true);
 
-    if (filter) {
-      query = `*[_type == "tag" && name == "${filter}"]{
-  _id, name,
-  "projects": *[_type == "projects" && references(^._id)]
-} | order(_createdAt desc)`
-      client.fetch(query)
-        .then(data => {
-          setPortfolioItems(data[0].projects)
-          setIsLoading(false)
-        })
-    } else {
-      query = '*[_type == "projects"] | order(_createdAt desc)'
-      client.fetch(query)
-        .then(data => {
-          setPortfolioItems(data)
-          setIsLoading(false)
-        })
-    }
-  }, [filter])
+        if ( filter ) {
+            query = `*[_type == "tag" && name == "${filter}"]{
+                _id, name,
+                "projects": *[_type == "projects" && references(^._id)]
+            } | order(_createdAt desc)`;
+            client.fetch(query)
+                .then(data => {
+                    setPortfolioItems(data[0].projects);
+                    setIsLoading(false);
+                });
+        } else {
+            query = '*[_type == "projects"] | order(_createdAt desc)';
+            client.fetch(query)
+                .then(data => {
+                    setPortfolioItems(data);
+                    setIsLoading(false);
+                });
+        }
+    }, [ filter ]);
 
-  const filterHandler = (filter) => {
-    setFilter(filter)
-  }
+    const filterHandler = (filter) => {
+        setFilter(filter);
+    };
 
-  return (
-    <main className="app__portfolio">
-      <h2 className="section-header">Portfolio</h2>
-      <div className="app__portfolio-tags">
-        {allTags?.map(tag => (
-          <button
-            className={tag?.name === filter ? "app__portfolio-tag active" : "app__portfolio-tag" }
-            key={tag?.name}
-            onClick={(e) => {
-              e.preventDefault()
-              e.stopPropagation()
-              filterHandler(tag?.name)
-            }}
-          >
-            <i className="ri-price-tag-3-line" /> {tag?.name}
-          </button>
-        ))}
-        <button
-          className={null === filter ? "app__portfolio-tag active" : "app__portfolio-tag" }
-          onClick={() => filterHandler(null)}
-        >
-          <i className="ri-price-tag-3-line" /> Show all
-        </button>
-      </div>
-      {isLoading ? (
-        <div className="app__portfolio-isLoading">
-          <Rings color='#F17A3B' />
-          <p>
-            Wait a second,<br/>
-              I'm loading the portfolio...
-          </p>
-        </div>
-      ) : (
-        <>
-          <div className="app__portfolio-items">
-            {portfolioItems?.map((item) => (
-              <PortfolioItem key={item.name} item={item} allTags={allTags} filterHandler={filterHandler} />
-            ))}
-          </div>
-        </>
-      )}
-    </main>
-  )
-}
+    return (
+        <main className="app__portfolio">
+            <h2 className="section-header">Portfolio</h2>
+            <div className="app__portfolio-tags">
+                {allTags?.map(tag => (
+                    <button
+                        className={`app__portfolio-tag ${filter === tag.name ? 'active' : ''}`}
+                        key={`all-tags-${tag?.name}`}
+                        onClick={() => {
+                            filterHandler(tag?.name);
+                        }}
+                    >
+                        <i className="ri-price-tag-3-line"/> {tag?.name}
+                    </button>
+                ))}
+                <button
+                    className={`app__portfolio-tag ${filter === null ? 'active' : ''}`}
+                    onClick={() => filterHandler(null)}
+                >
+                    <i className="ri-price-tag-3-line"/> Show all
+                </button>
+            </div>
+            {isLoading ? (
+                <div className="app__portfolio-is-loading">
+                    <Rings color="#F17A3B"/>
+                    <p>
+                        Wait a second,<br/>
+                        I'm loading the portfolio...
+                    </p>
+                </div>
+            ) : (
+                <>
+                    <div className="app__portfolio-items">
+                        {portfolioItems?.map((item) => (
+                            <PortfolioItem
+                                key={item.name}
+                                item={item}
+                                allTags={allTags}
+                                filterHandler={filterHandler}
+                            />
+                        ))}
+                    </div>
+                </>
+            )}
+        </main>
+    );
+};
 
-export default Portfolio
+export default Portfolio;
--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -1,62 +1,76 @@
-import React from 'react'
-import {PortableText} from '@portabletext/react'
+import React from 'react';
+import { PortableText } from '@portabletext/react';
 
-import './PortfolioItem.scss'
-import { urlFor } from '../../client'
+import './PortfolioItem.scss';
+import { urlFor } from '../../client';
 
-const PortfolioItem = ({item: {name, description, link, gitHub, image, tags}, allTags, filterHandler}) => {
-  function findTag(ref) {
-    return allTags.find(item => item._id === ref)
-  }
+const PortfolioItem = ({
+                           item: { name, description, link, gitHub, image, tags },
+                           allTags,
+                           filterHandler
+                       }) => {
+    function findTag(ref) {
+        return allTags.find(item => item._id === ref);
+    }
 
-  return (
-    <div className="app__project-card">
-      <div className="app__project-img">
-        <img src={urlFor(image).width(450).url()} alt={name}/>
-      </div>
-      <div className="app__project-info">
-        <h3 className="app__project-title">{name}</h3>
-        <div className="app__project-description">
-          <PortableText
-            value={description}
-          />
+    return (
+        <div className="app__project-card">
+            <div className="app__project-img">
+                <img
+                    src={urlFor(image)
+                        .width(450)
+                        .url()} alt={name}
+                    aria-label={name}
+                />
+            </div>
+            <div className="app__project-info">
+                <h3 className="app__project-title">{name}</h3>
+                <div className="app__project-description">
+                    <PortableText
+                        value={description}
+                    />
+                </div>
+                <div className="app__project-tags">
+                    {tags?.map((tag, i) => (
+                        <button
+                            className="app__project-tag"
+                            key={`project-tag-${i}`}
+                            onClick={() => filterHandler(findTag(tag._ref)?.name)}
+                            aria-label={`Filter by ${findTag(tag._ref)?.name}`}
+                        >
+                            <i className="ri-price-tag-3-line"/> {findTag(tag._ref)?.name}
+                        </button>
+                    ))}
+                </div>
+                <ul className="app__project-links">
+                    <li>
+                        <a
+                            href={gitHub}
+                            target="_blank"
+                            rel="noreferrer"
+                            title="GitHUB repository"
+                            className="app__project-link"
+                            aria-label="GitHUB repository"
+                        >
+                            <i className="ri-github-fill"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href={link}
+                            target="_blank"
+                            rel="noreferrer"
+                            title="Open project in new tab"
+                            className="app__project-link"
+                            aria-label="Open project in new tab"
+                        >
+                            Open project <i className="ri-external-link-line"/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div className="app__project-tags">
-          {}
-          {tags?.map((tag, i) => (
-            <button
-              className="app__project-tag"
-              key={i}
-              onClick={() => filterHandler(findTag(tag._ref)?.name)}
-              aria-label={`Filter by ${findTag(tag._ref)?.name}`}
-            >
-              <i className="ri-price-tag-3-line" /> {findTag(tag._ref)?.name}
-            </button>
-          ))}
-        </div>
-        <div className="app__project-links">
-          <a
-            href={gitHub}
-            target="_blank"
-            rel="noreferrer"
-            title="GitHUB repository"
-            className="app__project-link"
-          >
-            <i className="ri-github-fill" />
-          </a>
-          <a
-            href={link}
-            target="_blank"
-            rel="noreferrer"
-            title="Open project in new tab"
-            className="app__project-link"
-          >
-            Open project <i className="ri-external-link-line" />
-          </a>
-        </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default PortfolioItem
+export default PortfolioItem;
